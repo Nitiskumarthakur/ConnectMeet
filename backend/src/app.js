41772b76e1289eaf8a17854f3f9dfb39fda13fc3
Nitish kumar <kumarnitish95651@gmail.com>
2026-08-16 23:39:39 +0530

    Update CORS origin to new domain

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const io = connectToSocket(server);
 app.set("port", (process.env.PORT || 1600));
 
 app.use(cors({
-    origin: "https://connectmeeted-git-main-nitish-kumar05.vercel.app",
+    origin: "connectmeeted.vercel.app",
     methods:["GET", "POST"],
     credentials: true
 }));
